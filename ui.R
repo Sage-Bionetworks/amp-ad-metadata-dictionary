e51eeafeb4c07aea8078d7f475f038102cfb2460
Nicole Kauer <nmekauer@gmail.com>
2021-07-30 16:02:54 -0700

    Change 'or' to 'and' by Alex Knoll's request

--- ui.R
+++ ui.R
@@ -14,7 +14,7 @@ ui <- function() {
         p("Explore AD metadata terms by key, value, or descriptions. ",
           "You can use the large search bar to query the entire table, ",
           "the small search bars underneath the column names to query by ",
-          "column, or the pagination at the bottom to flip through the table."
+          "column, and the pagination at the bottom to flip through the table."
         )
       ),
       column(
