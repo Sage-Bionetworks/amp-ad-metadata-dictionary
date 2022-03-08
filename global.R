06fd99ef34cdaa6e926d2cb8295d909a8bc0f03e
thomasyu888 <thomas.yu@sagebase.org>
2022-03-08 13:32:09 +0800

    Begin removing synapse authentication

--- global.R
+++ global.R
@@ -6,6 +6,7 @@ suppressPackageStartupMessages(library("reticulate"))
 suppressPackageStartupMessages(library("shiny"))
 
 ## Load synapse client
+reticulate::use_condaenv("synapse")
 synapse <- reticulate::import("synapseclient")
 
 ## Function to truncate display in table
--- server.R
+++ server.R
@@ -1,46 +1,52 @@
 server <- function(input, output, session) {
   syn <- synapse$Synapse()
-  session$sendCustomMessage(type = "readCookie", message = list())
+  syn$setEndpoints(
+    repoEndpoint='https://repo-staging.prod.sagebase.org/repo/v1',
+    authEndpoint='https://auth-staging.prod.sagebase.org/auth/v1',
+    fileHandleEndpoint='https://file-staging.prod.sagebase.org/file/v1',
+    portalEndpoint='https://staging.synapse.org/'
+  )
+  # session$sendCustomMessage(type = "readCookie", message = list())
 
   ## Show message if user is not logged in to synapse
-  unauthorized <- observeEvent(input$authorized, {
-    showModal(
-      modalDialog(
-        title = "Not logged in",
-        HTML("You must log in to <a href=\"https://www.synapse.org/\">Synapse</a> to use this application. Please log in, and then refresh this page.")
-      )
-    )
-  })
-
-  observeEvent(input$cookie, {
-    is_logged_in <- FALSE
-    ## Use authToken and handle error here if still not logged in
-    tryCatch({
-      syn$login(sessionToken = input$cookie, silent = TRUE)
-      is_logged_in <- TRUE
-    },
-    error = function(err) {
-      showModal(
-        modalDialog(
-          title = "Login error",
-          HTML("There was an error with the login process. Please refresh your Synapse session by logging out of and back in to <a target=\"_blank\" href=\"https://www.synapse.org/\">Synapse</a>. Then refresh this page to use the application."), # nolint
-          footer = NULL
-        )
-      )
-    }
-    )
-    ## Check that user did not log in as anonymous
-    if (syn$username == "anonymous") {
-      showModal(
-        modalDialog(
-          title = "Login error",
-          HTML("There was an error with the login process. You have been logged in as anonymous."), # nolint
-          footer = NULL
-        )
-      )
-      is_logged_in <- FALSE
-    }
-    req(is_logged_in)
+  # unauthorized <- observeEvent(input$authorized, {
+  #   showModal(
+  #     modalDialog(
+  #       title = "Not logged in",
+  #       HTML("You must log in to <a href=\"https://www.synapse.org/\">Synapse</a> to use this application. Please log in, and then refresh this page.")
+  #     )
+  #   )
+  # })
+#
+#   observeEvent(input$cookie, {
+#     is_logged_in <- FALSE
+#     ## Use authToken and handle error here if still not logged in
+#     tryCatch({
+#       syn$login(sessionToken = input$cookie, silent = TRUE)
+#       is_logged_in <- TRUE
+#     },
+#     error = function(err) {
+#       showModal(
+#         modalDialog(
+#           title = "Login error",
+#           HTML("There was an error with the login process. Please refresh your Synapse session by logging out of and back in to <a target=\"_blank\" href=\"https://www.synapse.org/\">Synapse</a>. Then refresh this page to use the application."), # nolint
+#           footer = NULL
+#         )
+#       )
+#     }
+#     )
+    # ## Check that user did not log in as anonymous
+    # if (syn$username == "anonymous") {
+    #   showModal(
+    #     modalDialog(
+    #       title = "Login error",
+    #       HTML("There was an error with the login process. You have been logged in as anonymous."), # nolint
+    #       footer = NULL
+    #     )
+    #   )
+    #   is_logged_in <- FALSE
+    # }
+    # req(is_logged_in)
 
     ## Get annotations
     annots <- get_synapse_table(synID = "syn10242922", syn = syn)
@@ -94,5 +100,5 @@ server <- function(input, output, session) {
         )
       )
     })
-  })
+  # })
 }
